e125b92d86c3fa79041ebe05c8ee887b182beec5
kadia5 <kdabraeux@gmail.com>
2021-07-30 21:06:15 +0000

    timer1.js

---
+++ timer1.js
@@ -0,0 +1,30 @@
+const timerClock = function () {
+  
+  setTimeout(() => {
+    console.log('beep');
+  }, 3000);
+  setTimeout(() => {
+    console.log('beep');
+  }, 5000);
+  setTimeout(() => {
+    console.log('beep');
+  }, 9000);
+  setTimeout(() => {
+    console.log('beep');
+  }, 10000);
+  setTimeout(() => {
+    console.log('beep');
+  }, 15000);
+  setTimeout(() => {
+    console.log('');
+  }, "b");
+  setTimeout(() => {
+    console.log('');
+  }, -5);
+  setTimeout(() => {
+    console.log('');
+  }, );
+
+}
+timerClock()
+
